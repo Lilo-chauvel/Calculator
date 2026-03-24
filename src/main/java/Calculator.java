1881b44a7fc12d285cd9f092be5ac8f19d2e2982
theofnz26 <theo.fernandez@le-campus-numerique.fr>
2026-03-24 11:16:37 +0100

    feat: 4 method add in Calculator class

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,20 @@
 package java;
 
 public class Calculator {
+
+    public int addition (int a, int b){
+        return 0;
+    }
+
+    public int substract (int a, int b){
+        return 0;
+    }
+
+    public int multiply (int a, int b){
+        return 0;
+    }
+
+    public int devide (int a, int b){
+        return 0;
+    }
 }
